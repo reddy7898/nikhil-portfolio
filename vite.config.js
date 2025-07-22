307068e1850032c6f20568cd069c5cfcd5e5e684
Nikhil Duluri <nikhilreddy.duluri@gmail.com>
2025-07-22 18:04:33 +0530

    edited base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/nikhil_portfolio/',  // 👈 your repo name
+  base: '/nikhil-portfolio/',  // 👈 your repo name
   plugins: [react()]
 });
